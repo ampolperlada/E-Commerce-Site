5bfbafb59604d09a102841c1786864e72802c3e4
Ampol Perlada <ampolperlada@gmail.com>
2025-03-03 17:09:33 +0800

    debugging sidebar  Domain Not Resolved

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -22,7 +22,7 @@ const Sidebar = () => {
     useEffect(() => {
         const fetchCategories = async () => {
             try {
-                const response = await fetch('https://pixeljson.com/products');
+                const response = await fetch('https://jsonplaceholder.typicode.com/posts');
                 const data: FetchResponse = await response.json();
                 console.log(data);
             } catch (error) {
